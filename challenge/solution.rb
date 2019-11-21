4a599165988e9b91bf233e29f3a920db3887b5a8
tiagofsilva <aeroboom@gmail.com>
2019-11-21 04:17:55 -0300

    Finishes solution for challenge and refactors big time

--- challenge/solution.rb
+++ challenge/solution.rb
@@ -2,34 +2,35 @@ class_method_tuple = ENV['COUNT_CALLS_TO']
 matches = class_method_tuple.match(/(\w.*)(\.|\#)(.+)/)
 class_name, method_type, method_name = matches[1..3]
 is_singleton = method_type == '.'
-stop_recursion = false
 count = 0
 
+def class_name_for(receiver)
+  (receiver.is_a?(Class) || receiver.is_a?(Module)) ? receiver.name : receiver.class.name
+end
+
+def singleton_method?(klass, method_name)
+  klass.singleton_methods(false).include?(method_name.to_sym)
+end
+
+def class_or_module?(unit)
+  unit.is_a?(Module) || unit.is_a?(Class)
+end
+
 define_method :alias_and_override_method do |klass, mname|
-  if is_singleton
-    klass.singleton_class.class_eval do
+  owner_class = is_singleton ? klass.singleton_class : klass
+  owner_class.class_eval do
+    if !instance_methods(false).include?(:"old_#{mname}") && method_defined?(mname)
       alias_method "old_#{mname}", mname
       define_count_method(mname)
     end
-  else
-    klass.class_eval do
-      if !self.instance_methods(false).include?("old_#{mname}")
-        alias_method "old_#{mname}", mname
-        define_count_method(mname)
-      end
-    end
   end
 end
 
-define_method :define_count_method do |method_name|
-  define_method method_name do |*args, &block|
+define_method :define_count_method do |mname|
+  define_method mname do |*args, &block|
     count = count.succ
-    send("old_#{method_name}", *args, &block)
+    send("old_#{mname}", *args, &block)
   end
-end
-
-def class_name_for(receiver)
-  receiver.is_a?(Class) ? receiver.name : receiver.class.name
 end
 
 begin
@@ -37,21 +38,20 @@ begin
   alias_and_override_method(klass, method_name)
 rescue NameError
   adder = lambda do |method|
-    if method.to_s == method_name && self.name == class_name && !stop_recursion
-      stop_recursion = true
+    if method.to_s == method_name && self.name == class_name
       alias_and_override_method(self, method_name)
     end
   end
 
   includer = lambda do |receiver|
-    if class_name_for(receiver) == class_name && !stop_recursion
-      stop_recursion = true
-      alias_and_override_method(receiver, method_name)
+    receiver_class = class_or_module?(receiver) ? receiver : receiver.class
+    if receiver_class.name == class_name
+      alias_and_override_method(receiver_class, method_name)
     end
   end
 
   inheriter = lambda do |receiver|
-    if receiver.name == class_name && receiver.superclass.instance_methods.include?(method_name.to_sym)
+    if receiver.name == class_name
       alias_and_override_method(receiver.superclass, method_name)
     end
   end
